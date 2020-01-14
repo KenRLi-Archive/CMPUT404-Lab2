342a63c87cf70839c91a7d5f12c092647224bd27
Ken Li <krli@ualberta.ca>
2020-01-14 08:44:46 -0700

    Fixed errors

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
 import socket
 
-HOST = "www.google.com'"
+HOST = "www.google.com"
 PORT = 80
-
-REQUEST = b"GET / HTTP/1.0\r\n\r\n"
+PAYLOAD = "GET / HTTP/1.0\r\nHost: " + HOST + "\r\n\r\n"
+BUFFER_SIZE = 4096
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+s.sendall(PAYLOAD.encode())
+
+data = s.recv(BUFFER_SIZE) 
+print(data)
+
 s.close()
