1e4ef32fbfb8c0708d62e537d7c470790660f3e4
Ken Li <krli@ualberta.ca>
2020-01-14 10:26:06 -0700

    Added multiprocess versions of echo and proxy

---
+++ multi_echo_server.py
@@ -0,0 +1,31 @@
+import socket
+import time
+from multiprocessing import Process
+
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(2)
+
+        while(True):
+            conn, addr = s.accept()
+            p = Process(target=handleEcho, args=(conn, addr))
+            p.daemon = True
+            p.start()
+            print("Started process", p)
+
+def handleEcho(conn, addr):
+    print("Connected by", addr)
+
+    fullData = conn.recv(BUFFER_SIZE)
+    conn.sendall(fullData)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close
+
+if __name__ == "__main__":
+    main() 
