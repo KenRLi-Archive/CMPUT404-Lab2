1b87966148a8316bd324ffda5f92c70049839114
Ken Li <krli@ualberta.ca>
2020-01-14 09:29:17 -0700

    Fixed typos

--- echo_server.py
+++ echo_server.py
@@ -3,18 +3,18 @@ import time
 
 HOST = ""
 PORT = 8001
-BUFFER = 1024
+BUFFER_SIZE = 1024
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsocket(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen(2)
 
-        while():
+        while(True):
             conn, addr = s.accept()
             print("Connected by", addr)
-            fullData = conn.recv(BUFFER)
+            fullData = conn.recv(BUFFER_SIZE)
             time.sleep(0.5)
             conn.sendall(fullData)
             conn.close
